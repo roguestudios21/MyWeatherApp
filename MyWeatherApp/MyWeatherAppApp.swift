04bc85d8f22e28ebcb21e20894cb08cd70541115
roguestudios21 <atharv.choughule@gmail.com>
2025-07-07 09:36:15 +0530

    Initial Commit

---
+++ MyWeatherApp/MyWeatherAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyWeatherAppApp.swift
+//  MyWeatherApp
+//
+//  Created by Atharv  on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
